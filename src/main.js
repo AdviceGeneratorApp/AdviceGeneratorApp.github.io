f178b6d45d5e0151d10e501b3c3ee1fd94e1c3f6
AdviceGeneratorApp <144510189+AdviceGeneratorApp@users.noreply.github.com>
2023-09-09 12:43:38 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ const advice = document.querySelector('.advice')
 const adviceNumber = document.querySelector('.title span')
 const button = document.querySelector('.button')
 
-//console.log({"slip": { "id": 171, "advice": "If you've nothing nice to say, say nothing."}})
-
 function generateAdvice() {
     fetch('https://api.adviceslip.com/advice')
         .then(res => res.json())
